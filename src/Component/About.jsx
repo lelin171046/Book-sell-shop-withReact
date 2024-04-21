a521122bbc0226ea30fc40700adf61c3d8f577fe
lelin171046 <lelin171046@gmail.com>
2024-04-22 03:35:51 +0600

    add the about section

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -4,8 +4,13 @@ const About = () => {
 	return (
 		<section className="py-6 dark:bg-violet-600 dark:text-gray-50">
 			<div className="container mx-auto flex flex-col items-center justify-center p-4 space-y-8 md:p-10 md:px-24 xl:px-48">
-				<h1 className="text-5xl font-bold leading-none text-center">Something totally different</h1>
-				<p className="pt-2 pb-8 text-xl font-medium text-center">Eum omnis itaque harum at quae sequi unde similique alias asperiores totam. Ex cumque maxime harum doloremque, tempore nam fugiat aspernatur rerum ipsa unde est modi commodi molestias maiores eveniet eius esse asperiores neque dicta ea quisquam? Excepturi sapiente officiis explicabo ab?</p>
+				<h1 className="text-5xl font-bold leading-none text-center">Welcome to our book-reading website!</h1>
+				<div className="pt-2 pb-8 text-xl font-medium text-center">
+					<p> We are passionate about connecting readers with great books.</p>
+					<p>Our mission is to provide a platform where book lovers can discover new titles, connect with authors, and share their love for reading.</p>
+					<p>Whether you're looking for bestsellers, classics, or hidden gems, we've got something for everyone.</p>
+					<p>Thank you for visiting our site and happy reading!</p>
+				</div>
 				<button className="px-8 py-3 text-lg font-semibold rounded dark:bg-gray-100 dark:text-gray-900">Learn more</button>
 			</div>
 		</section>
